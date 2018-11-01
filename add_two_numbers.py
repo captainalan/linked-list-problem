0d637eb2e9727f1844b4be9480c2d9f6a773f743
Alan Wong <captainalan@gmail.com>
2018-10-31 20:57:43 -0700

    Added tests

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -17,15 +17,24 @@ class ListNode:
         self.next = None
 
 class Solution:
+    # This is the function we were asked to write in the problem
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        sum = ListNode_to_int(l1) + ListNode_to_int(l2)
+        return int_to_ListNode(sum)
 
-    # Helper Functions
-    def ListNode_to_int(MyListNode, ten_power=0, current_val=0):
-        foo = (MyListNode.val * 10**ten_power)
-        if MyListNode.next == None: # Base Case
-            return foo + current_val
-        else: # Recursive case
-            return ListNode_to_int(MyListNode.next, 
-                    ten_power+1, current_val+foo) 
+# Helper Functions
+def ListNode_to_int(MyListNode, ten_power=0, current_val=0):
+    foo = (MyListNode.val * 10**ten_power)
+    if MyListNode.next == None: # Base Case
+        return foo + current_val
+    else: # Recursive case
+        return ListNode_to_int(MyListNode.next, 
+                ten_power+1, current_val+foo) 
 
 def int_to_ListNode(MyInt, prev_node=None):
     foo = mod_ten = MyInt % 10
@@ -38,17 +47,5 @@ def int_to_ListNode(MyInt, prev_node=None):
         bar.next = int_to_ListNode(MyInt // 10, bar) # Add node
         return bar
 
-
-# This is the function we were asked to write in the problem
-def addTwoNumbers(self, l1, l2):
-    """
-    :type l1: ListNode
-    :type l2: ListNode
-    :rtype: ListNode
-    """
-    sum = ListNode_to_int(l1) + ListNode_to_int(l2)
-    return int_to_ListNode(sum)
-
 def __init__():
     pass
-
--- tests.py
+++ tests.py
@@ -1,38 +1,57 @@
 import time
 import unittest
+from add_two_numbers import * # Just testing one small file!
 
-from add_two_numbers import ListNode, Solution
+"""For testing that ListNodes <-> int Conversions work"""
+three = int_to_ListNode(3)
+thirty_three = int_to_ListNode(33)
 
-"""
-Some test case stuff
-"""
-
-# Set up the example that we are given
+"""Set up the example that we are given"""
+# 342
 n1 = ListNode(2)
 n2 = ListNode(4)
 n3 = ListNode(3)
-
 n1.next = n2
 n2.next = n3
 
+# 465
 m1 = ListNode(5)
 m2 = ListNode(6)
 m3 = ListNode(4)
-
 m1.next = m2
 m2.next = m3
 
-# Run the tests
-sol = Solution()
+class MyTest(unittest.TestCase):
+    """
+    Tests
 
-# Converting two and from
-three = int_to_ListNode(3)
-print(ListNode_to_int(three))
+    Example input/output from problem statement reproduced here.
 
-thirty_three = int_to_ListNode(33)
-print(ListNode_to_int(thirty_three))
+    Input: (2 -> 4 -> 3) + (5 -> 6  -> 4)
+    Output: 7 -> 0 -> 8
+    Explanation: 342 + 465 = 807
+    """
 
-print("The example given in this question: ")
-print(ListNode_to_int(sol.addTwoNumbers(n1, m1)))
+    def test_one_digit(self):
+        self.assertEqual(ListNode_to_int(three), 3)
 
+    def test_two_digits(self):
+        self.assertEqual(ListNode_to_int(thirty_three), 33)
 
+    def test_three_digits(self):
+        self.assertEqual(ListNode_to_int(n1), 342)
+        self.assertEqual(ListNode_to_int(m1), 465)
+
+    def test_adding_solution(self):
+        sol = Solution()
+        # Example from problem statement
+        self.assertEqual(ListNode_to_int(sol.addTwoNumbers(n1, m1)), 807)
+
+        # Some bigger numbers
+        over9000_1 = ListNode(123456789)
+        over9000_2 = ListNode(987654321)
+        self.assertEqual(ListNode_to_int(sol.addTwoNumbers(over9000_1,
+            over9000_2)), (123456789 + 987654321))
+
+if __name__ == '__main__':
+    unittest.main()
